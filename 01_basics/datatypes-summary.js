153c89d5d93b16a0536b823a786861cb9ebbd0d4
Sahil Sonar <152708578+SahilSonar-04@users.noreply.github.com>
2025-07-03 15:01:56 +0530

    Update datatypes-summary.js
    Added comments

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -16,7 +16,24 @@ console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
 
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+// JavaScript is a dynamically typed language. This means that variable types are determined at runtime, 
+// and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime
 
 // Reference (Non primitive)
 
