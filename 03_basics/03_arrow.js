b8d7e355e935d4aa71ce405387be24162dff9156
Sahil Sonar <152708578+SahilSonar-04@users.noreply.github.com>
2025-07-27 22:17:36 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -17,36 +17,47 @@ const user = {
 // Browser ke andar global object is window object , isliye udhar output hoga window , but yaha empty object   {} hai
 // console.log(this);
 
-//This object ke andar use karo , function ke andar nahi
+// 'This' object ke andar use karo , function ke andar nahi
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // returns undefined
+// }
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this); // returns a lot of things
 // }
 
 // chai()
 
+
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // returns undefined
+// }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this.username); // returns undefineds
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); // returns empty paranthesis
 }
 
 
 // chai()
 
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
+//     return num1 + num2    // explicit return matlab return likhna padta hai
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2   // This is called implicit return jaha return likhne ki jarurat nahi hai in one line statement
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 ) // {} me return likhne padega , () me nahi
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) // object return bass paranthesis ke andar hi hoga
 
 
 console.log(addTwo(3, 4))
