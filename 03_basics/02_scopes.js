adf6c5775d9d4e5782fa1263686d8434ff3edad3
Sahil Sonar <152708578+SahilSonar-04@users.noreply.github.com>
2025-07-27 21:53:40 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -44,15 +44,23 @@ if (true) {
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
+
+//Function Hoisting
+//In JavaScript, function declarations are hoisted to the top of their scope during compilation phase. That means:
+//The entire function definition is moved to the top internally before the code runs.
+//So, even though the function is written after the call, it’s already known to the interpreter.
 console.log(addone(5))
 
+// this is a function , y
 function addone(num){
     return num + 1
 }
 
 
-
+//function hoisting not valid for expression
 addTwo(5)
+
+// this is an expression
 const addTwo = function(num){
     return num + 2
 }
