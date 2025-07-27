0a15ed5675d27e59e359fa9ff2dcc12baa3054d3
Sahil Sonar <152708578+SahilSonar-04@users.noreply.github.com>
2025-07-27 22:31:17 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,9 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// We may face problems due to pollution in global scope , so to solve this we use iife end me and function ke around () laga do bass
+// IIFE use karke function immediately execute ho jae
 
+// iife use karne ke baadcontext kab end hota hai yeh code ko pata nahi so use semicolon at end warna aage error aaega
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
